faf78e9e4ed93b43f94dbe0aeb396c92ac34f74e
fasil03 <fasilalemye@gmail.com>
2025-10-10 08:25:44 +0200

    .

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ const maxScoreSpan=document.getElementById("max-score")
 const resultMesssage=document.getElementById("result=message")
 const restartButton=document.getElementById("restart-btn")
 const progressbar=document.getElementById("progress")
+const quizQuestios=[
+    {
+    question:"what is the capital of Ethiopia?",
+    answers: [
+        {text:"bahir dar",correct: false},
+        {text:"gondar",correct: false},
+        {text:"metema",correct: false},
+        {text:"addis abeba",correct: true},
+   ],
+ },
+
+]
+let currentQuestionIndex=onabort;
+let score=0;
+let answerDisabled=false;
+
+totalQuestionSpan.textContent=quizQuestios.length;
+maxScoreSpan.textContent=quizQuestios.length;
+//event listener
+startButton.addEventListener("click",startQuiz);
+restartButton.addEventListener("click",restartQuiz);
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    scoreSpan.textContent=score;
+    startScreen.classList.remove("active");
+    quizScreen.classList.add("active");
+
+}
+function restartQuiz(){
+
+}
+
